223be900aa98e4e471edfa6ba2faf9be74c2aa9e
zhoujiahao_glb <1836418597@qq.com>
2020-12-15 20:02:47 +0800

    kmall-user-service

---
+++ kmall-user-service/src/test/java/com/kgc/kmall/user/KmallUserServiceApplicationTests.java
@@ -0,0 +1,25 @@
+package com.kgc.kmall.user;
+
+import com.kgc.kmall.user.bean.Member;
+import com.kgc.kmall.user.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class KmallUserServiceApplicationTests {
+
+	@Resource
+	MemberService memberService;
+	
+	@Test
+	void contextLoads() {
+        List<Member> members = memberService.selectAllMember();
+        for (Member member : members) {
+            System.out.println(member.toString());
+        }
+    }
+
+}
